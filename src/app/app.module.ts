3857a295ea6a88b654ed0c171fd50c1918514850
Wiktor <wiktorkalinowski@gmail.com>
2017-08-29 21:48:07 +0200

    Navigation and Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {FormsModule} from "@angular/forms";
+import {FormsModule} from '@angular/forms';
 
 import {AppComponent} from './app.component';
 import {ProductListComponent} from './products/product-list-component';
-import {ConvertToSpacesPipe} from "./shared/convert-to-spaces.pipe";
-import {StarComponent} from "./shared/star.component";
-import {HttpClientModule} from "@angular/common/http";
-import {WelcomeComponent} from "./home/welcome.component";
+import {ConvertToSpacesPipe} from './shared/convert-to-spaces.pipe';
+import {StarComponent} from './shared/star.component';
+import {HttpClientModule} from '@angular/common/http';
+import {WelcomeComponent} from './home/welcome.component';
+import {ProductDetailComponent} from './products/product-detail.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -15,12 +17,21 @@ import {WelcomeComponent} from "./home/welcome.component";
     ProductListComponent,
     ConvertToSpacesPipe,
     StarComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    ProductDetailComponent,
+    RouterModule
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'product', component: ProductListComponent},
+      {path: 'products/id', component: ProductDetailComponent},
+      {path: 'welcome', component: WelcomeComponent},
+      {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+      {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
